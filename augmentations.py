40fc2748770777f0be8780e1612751ccde808abf
kdobolyi <39930627+kdobolyi@users.noreply.github.com>
2020-07-31 09:45:00 -0700

    readability cleaning

--- augmentations.py
+++ augmentations.py
@@ -1,12 +1,13 @@
-# #########################################################################################################################################################
+# #####################################################################################################################
 '''
 
-This module provides various transformations their helper functions to apply to your dataset as augmentations during training.
+This module provides various transformations their helper functions to apply to your dataset as augmentations during 
+training.
 
 TODO: remove the writing and reading from dummy image files for debugging, to make the transformations faster
 
 '''
-# #########################################################################################################################################################
+# #####################################################################################################################
 
 # image processing
 from skimage.feature import local_binary_pattern
@@ -26,14 +27,20 @@ import torch.nn as nn
 import numpy 
 import os
 
-# find the mean and stddev for the images, to be used later in further transforms
-# this can take a while to run, so we have the function quit, and you can copy its results into a config manually
+
 def getMeanStddev(loader):
+    """ Find the mean and stddev for the images, to be used later in further transforms
+        this can take a while to run, so we have the function quit, and you can copy its results into a config manually
+
+    Args:
+        loader: A DataLoader object containing your image dataset. The code below will just use the first index of 
+                whatever your loader returns. (So it can handle filenames of images if you want to use them.)
+    """
     mean = 0.
     std = 0.
     nb_samples = 0.
     for data in loader:
-        data = data[0]  # in case the dataloader also wants to pass filenames, we ignore these by selecting just the first column
+        data = data[0]  
         batch_samples = data.size(0)
         print(batch_samples)
         data = data.view(batch_samples, data.size(1), -1)
@@ -48,8 +55,8 @@ def getMeanStddev(loader):
     print('std ', std)
     quit()
 
-# some slides might be sharper than others; try to blur the sharp ones sometimes?
 class RandomGaussianBlur:
+    """ some slides might be sharper than others; try to blur the sharp ones sometimes? """
     def __init__(self, multichannel=True):
         self.multichannel = multichannel
 
@@ -58,8 +65,8 @@ class RandomGaussianBlur:
         openCVim = cv2.GaussianBlur(openCVim,(5,5),0)
         return Image.fromarray(openCVim)
 
-# try to binarize the image in various ways (most useful if we were doing cell segmentation)
 class ApplyThreshold:
+    """ try to binarize the image in various ways (most useful if we were doing cell segmentation) """
     def __init__(self, typeThreshold='binary', adaptive=False, multichannel=True, cutoff=5):
         self.multichannel = multichannel
         self.typeThreshold = typeThreshold
@@ -69,18 +76,22 @@ class ApplyThreshold:
     def __call__(self, x):
         open_cv_image = numpy.array(x) 
 
-        # convert to B&W (TODO: there is probably a more elegant way to do this -- want code to work regardless if original was greyscale or not)
+        # convert to B&W (TODO: there is probably a more elegant way to do this -- want code to work regardless if 
+        # original was greyscale or not)
         Image.fromarray(open_cv_image).save('./temp.tif')
         open_cv_image = cv2.imread('./temp.tif', cv2.IMREAD_GRAYSCALE)
 
         if self.adaptive:
             if self.typeThreshold == 'binary':
-                open_cv_image = cv2.adaptiveThreshold(open_cv_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,7,self.cutoff)
+                open_cv_image = cv2.adaptiveThreshold(open_cv_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                    cv2.THRESH_BINARY,7,self.cutoff)
             elif self.typeThreshold == 'mean':
-                open_cv_image = cv2.adaptiveThreshold(open_cv_image,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,7,self.cutoff)
+                open_cv_image = cv2.adaptiveThreshold(open_cv_image,255,cv2.ADAPTIVE_THRESH_MEAN_C,
+                    cv2.THRESH_BINARY,7,self.cutoff)
             # TODO: why is this broken for otsu?
             #elif self.typeThreshold == 'otsu':
-            #    open_cv_image = cv2.adaptiveThreshold(open_cv_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY+cv2.THRESH_OTSU,7,5)
+            #    open_cv_image = cv2.adaptiveThreshold(open_cv_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+            #       cv2.THRESH_BINARY+cv2.THRESH_OTSU,7,5)
 
         else:
             if self.typeThreshold == 'binary':
@@ -92,9 +103,9 @@ class ApplyThreshold:
 
         return Image.fromarray(open_cv_image).convert('RGB')
 
-# this is the same as Openingl once again, trying to get the "interesting" parts of an image seperated from "debris"
-# probably more useful if, again, we were trying to do some kind of segmentation
 class ErosionAndDilation:
+    """ this is the same as Openingl once again, trying to get the "interesting" parts of an image seperated from "debris"
+        probably more useful if, again, we were trying to do some kind of segmentation """
     def __init__(self, multichannel=True):
         self.multichannel = multichannel
 
@@ -108,8 +119,9 @@ class ErosionAndDilation:
         openCVim = cv2.morphologyEx(openCVim, cv2.MORPH_OPEN, kernel)
         return Image.fromarray(openCVim)
 
-# an intelligent way to emphasize finer details in an image when trying to binarize (which it could/should be combined with)
 class TopHat:
+    """ an intelligent way to emphasize finer details in an image when trying to binarize (which it could/should be 
+        combined with) """
     def __init__(self, multichannel=True):
         self.multichannel = multichannel
 
@@ -123,8 +135,8 @@ class TopHat:
         openCVim = cv2.morphologyEx(openCVim, cv2.MORPH_TOPHAT, kernel)
         return Image.fromarray(openCVim)
 
-# detects and emphasizes edges
 class SobelX:
+    """ detects and emphasizes edges """
     def __init__(self, multichannel=True):
         self.multichannel = multichannel
 
@@ -136,8 +148,8 @@ class SobelX:
         openCVim = cv2.Sobel(openCVim,cv2.CV_64F,1,0,ksize=5)
         return Image.fromarray((openCVim * 255).astype(numpy.uint8))
 
-# increases the contrast in an image through a histogram
 class ContrastThroughHistogram:
+    """ increases the contrast in an image through a histogram """
     def __init__(self, multichannel=True):
         self.multichannel = multichannel
 
@@ -157,8 +169,9 @@ class ContrastThroughHistogram:
         openCVim = cv2.imread('./temp.tif')
         return Image.fromarray(openCVim)
 
-# converts the image to an array of integers, one per pixel, that are calculated using local histograms of brightness of pixels of some radius
 class LocalBinaryPattern:
+    """ converts the image to an array of integers, one per pixel, that are calculated using local histograms of 
+        brightness of pixels of some radius """
     def __init__(self, multichannel=True):
         self.multichannel = multichannel
 
@@ -175,8 +188,9 @@ class LocalBinaryPattern:
         openCVim = local_binary_pattern(openCVim, n_points, radius, METHOD)
         return Image.fromarray(openCVim).convert('RGB')
 
-# converts an RBG image to greyscale, and then back to RBG to be compatible with various transforms and/or transfer learning (which requires RBG)
 class Grayscale:
+    """ converts an RBG image to greyscale, and then back to RBG to be compatible with various transforms and/or 
+        transfer learning (which requires RBG) """
     def __init__(self, multichannel=True):
         self.multichannel = multichannel
 
@@ -184,8 +198,9 @@ class Grayscale:
         x = x.convert('L')
         return x
 
-# converts an RBG image to greyscale, and then back to RBG to be compatible with various transforms and/or transfer learning (which requires RBG)
 class RGB:
+    """ converts an RBG image to greyscale, and then back to RBG to be compatible with various transforms and/or 
+        transfer learning (which requires RBG) """
     def __init__(self, multichannel=True):
         self.multichannel = multichannel
 

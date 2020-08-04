5c3f62c0a53af425543e56d3a0ae01ff6c690ea1
kdobolyi <39930627+kdobolyi@users.noreply.github.com>
2020-08-04 14:46:25 -0700

    cosmetic cleanup

--- cellnet_driver_no_validation.py
+++ cellnet_driver_no_validation.py
@@ -1,4 +1,4 @@
-# #########################################################################################################################################################
+# #####################################################################################################################
 '''
 
 This module is used to test one or more models+data specified in config files on a CV framework.
@@ -7,7 +7,8 @@ For the BioNic project, we will be running the following experiments across seve
 1. Investigate how transfer learning improves models (or not)
 2. Investigate what are the best data pre-processing and augmentations to use
 3. Investigate if transfer learning from CellNet is better than transfer learning from ImageNet
-4. Use VennData to make measurements about the quality of our datasets, and see if we can get better results with that knowledge
+4. Use VennData to make measurements about the quality of our datasets, and see if we can get better results with that 
+   knowledge
 
 Future work for this project includes:
 1. Implement something to automatically resize an image to a standard cell size 
@@ -16,7 +17,7 @@ Future work for this project includes:
 4. Active learning?
 
 '''
-# #########################################################################################################################################################
+# #####################################################################################################################
 
 # PyTorch
 from torchvision import datasets, models
@@ -44,23 +45,24 @@ from augmentations import *
 from augment_config import *
 from cellnet_config import *
 
-# #########################################################################################################################################################
+# #####################################################################################################################
 # BASIC CONFIGURATION
-# #########################################################################################################################################################
+# #####################################################################################################################
 batch_size = 8
-epochs = 15             # default epochs (can be overriden in model config)
-learning_rate = 1e-4
+epochs = 20             # default epochs (can be overriden in *_config.py for your models)
+learning_rate = 1e-3
 device = "cuda"
 
-calcStats = False       # turn this on if you want to calc mean and stddev of training dataset for normalization in transform
-cvFolds = 5             # how many cross validation folds to use
-repeats = 4             # how many times you want to repeat the experiment (so total runs will be cvFolds * repeats)
-drop_last = True        # BatchNorm has issues if the samples aren't in the min_size of the batch, or the batch has one sample in it
-non_CV_div = 1          # used to reduce the number of trials manually if you're not doing CV
+calcStats = False  # turn this on if you want to calc mean and stddev of training dataset for normalization in transform
+cvFolds = 5        # how many cross validation folds to use
+repeats = 4        # how many times you want to repeat the experiment (so total runs will be cvFolds * repeats)
+drop_last = True   # BatchNorm has issues if there are too few samples in the final batch; this drops those offenders
+non_CV_div = 1     # used to reduce the number of trials manually if you're not doing CV
 
 SINGLE_IMAGE = '/home/kdobolyi/cellnet/activations/' # used for printing out activations (for a blog post)
 
-# used with calcStats to calculate the mean and stddev of the greyscale images for later normalization; you may need to update this for your dataset
+# used with calcStats to calculate the mean and stddev of the greyscale images for later normalization; you may need to 
+# update this for your dataset
 stats_transforms = [#transforms.CenterCrop(224),
                     #transforms.Grayscale(),
                     transforms.Resize([224,224]),
@@ -85,10 +87,11 @@ if calcStats == False:
     # if you want to overrride the default number of epochs specified at the top of this file
     if 'epochs' in model.keys():
         epochs = model['epochs']
-        scoring = "_" + str(epochs) + "epochs_lr" + str(learning_rate).replace("0.", "_")            # "_recall", etc
-        model['file_label'] = scoring
+    scoring = "_" + str(epochs) + "epochs_lr" + str(learning_rate).replace("0.", "_")            # "_recall", etc
+    model['file_label'] = scoring
 
-    # you will set this to True the first time you run this code on a new dataset to generate a global holdout, then set it to False
+    # you will set this to True the first time you run this code on a new dataset to generate a global holdout, then 
+    # set it to False
     generateHoldout = eval(sys.argv[3])
 
 else:
@@ -97,9 +100,9 @@ else:
     loader = DataLoader(dataset, batch_size=batch_size, num_workers=0, shuffle=False)
     getMeanStddev(loader)
 
-# #########################################################################################################################################################
+# #####################################################################################################################
 # DATASET PREP
-# #########################################################################################################################################################
+# #####################################################################################################################
 
 # set up the image transformations to be applied to all models that will be run
 image_transforms = {
@@ -113,8 +116,25 @@ aggregateResults = "./dataframe_" + model['name'] + scoring + ".csv"
 if os.path.exists(aggregateResults):
     os.system("rm " + aggregateResults)
 
-# define the basic train and test idiom
-def runTrainAndTest(train_dataset, test_dataset, dataloaders, f, results_normal):
+def runTrainAndTest(train_dataset, test_dataset, dataloaders, f_label, results_normal):
+    """define the basic train and test idiom
+
+    Args:
+        train_dataset: ImageFolderWithPaths for the training images; will be used to create a WeightedRandomSampler for 
+            the DataLoader
+        test_dataset: ImageFolderWithPaths for the testing images
+        dataloaders: dict of DataLoader objects we'll modify here in this function
+        f_label: a user-chosen label to include in the name of the .torch files generated during training
+        results_normal: a dictionary to be updated with the targets and predictions from this test run; collected for 
+            voting algorithms later
+
+
+    Returns:
+        confidence_mapping: a zip of image file paths, prediction confidences, target labels, and prediction labels 
+            for later reporting and analysis
+
+    """
+
     # https://discuss.pytorch.org/t/balanced-sampling-between-classes-with-torchvision-dataloader/2703/26                                                                        
     # For unbalanced dataset we create a weighted sampler                       
     weights = make_weights_for_balanced_classes(train_dataset.imgs, len(train_dataset.classes))                                                                
@@ -130,7 +150,7 @@ def runTrainAndTest(train_dataset, test_dataset, dataloaders, f, results_normal)
     print("training model " + str(ctor) + " on fold...")
     neurons = eval(ctor) # we specified the constructor in the config file as a string, so we need to eval it here
     print("Number of traininable parameters: ", neurons.count_parameters())
-    neurons.train(dataloaders['train'], None, epochs, device, model, f)
+    neurons.train(dataloaders['train'], None, epochs, device, model, f_label)
     
     print("testing model on fold...")
     neurons.test(dataloaders['test'], device, model, aggregateResults, "test")
@@ -148,8 +168,8 @@ if model['usesCV']:
     if generateHoldout: # should only be called once at the start of a new dataset experiment
         makeGlobalHoldout(model)
     holdout_normal = ImageFolderWithPaths(root="HOLDOUT_"+model['traindir'], transform=image_transforms['test'])
-    #holdout_normal = ImageFolderWithPaths(root=SINGLE_IMAGE, transform=image_transforms['test'])   # if we want to evaluate the trained model 
-                                                                                                    # on just a single image, for blog post purposes
+    # if we want to evaluate the trained model on just a single image, for blog post purposes
+    #holdout_normal = ImageFolderWithPaths(root=SINGLE_IMAGE, transform=image_transforms['test'])   
     dataloaders = {'holdout_normal': DataLoader(holdout_normal, batch_size=batch_size, shuffle=False)}
 
     # prepare the directory structure and recording for all iterations of this model training on its dataset
